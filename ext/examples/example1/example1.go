3e8ca9e3c25655584798b4496f048c104583a426
Daniel Hess <dan9186@gmail.com>
2015-04-08 03:04:18 -0700

    simplifying example1 as much as possible

--- ext/examples/example1/example1.go
+++ ext/examples/example1/example1.go
@@ -5,28 +5,9 @@ import (
 	"net/http"
 
 	_ "github.com/gomicro/steward"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	go startServiceLoop()
+	go http.ListenAndServe(net.JoinHostPort("0.0.0.0", "8000"), nil)
 	select {}
-}
-
-func registerEndpoints() http.Handler {
-	r := mux.NewRouter()
-	r.Path("/foo").HandlerFunc(FooHandler).Methods("GET")
-	return r
-}
-
-func startServiceLoop() {
-	handler := registerEndpoints()
-	http.Handle("/", handler)
-	http.ListenAndServe(net.JoinHostPort("0.0.0.0", "8000"), nil)
-}
-
-func FooHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Foobar"))
 }
