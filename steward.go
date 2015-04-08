2247cdf36b80034632c94de74a8854735c85d09d
Daniel Hess <dan9186@gmail.com>
2015-04-08 02:48:44 -0700

    allowing for custom json

--- steward.go
+++ steward.go
@@ -3,24 +3,15 @@ package steward
 import (
 	"encoding/json"
 	"net/http"
-	"os"
-	"strings"
 )
 
 var (
 	statusEndpoint = "/status"
-	resp           *Response
+	resp           interface{}
 )
 
 func init() {
-	appStr := strings.Split(os.Args[0], "/")
-	app := appStr[len(appStr)-1]
-	resp = &Response{app}
 	http.Handle(statusEndpoint, http.HandlerFunc(Status))
-}
-
-type Response struct {
-	Application string `json:"app"`
 }
 
 func Status(w http.ResponseWriter, req *http.Request) {
@@ -32,4 +23,8 @@ func Status(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write(payload)
+}
+
+func StatusResponse(v interface{}) {
+	resp = v
 }
