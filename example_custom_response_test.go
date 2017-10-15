d7135d6b930eef1ef5f27ddf3f89903b4e44d681
Daniel Hess <dan9186@gmail.com>
2017-10-15 00:37:35 -0700

    moving examples from files to godoc examples

---
+++ example_custom_response_test.go
@@ -0,0 +1,23 @@
+package steward_test
+
+import (
+	"net"
+	"net/http"
+
+	. "github.com/gomicro/steward"
+)
+
+// StatusResponse is the data for the status endpoint to display
+type StatusResponse struct {
+	Application string `json:"app"`
+	Version     string `json:"version"`
+	BuildTime   string `json:"buildTime"`
+}
+
+// ExampleCustomResponse demonstrates how to configure a custom response
+func Example_customResponse() {
+	SetStatusResponse(&StatusResponse{Application: "Foo", Version: "1.0.0", BuildTime: "Today"})
+
+	go http.ListenAndServe(net.JoinHostPort("0.0.0.0", "8000"), nil)
+	select {}
+}
---
+++ example_dropin_test.go
@@ -0,0 +1,19 @@
+package steward_test
+
+import (
+	"net"
+	"net/http"
+
+	_ "github.com/gomicro/steward"
+)
+
+var (
+	host = "0.0.0.0"
+	port = "8000"
+)
+
+// ExampleDropin demonstrates how to start a service with steward automatically injecting a status endpoint
+func Example_dropin() {
+	go http.ListenAndServe(net.JoinHostPort(host, port), nil)
+	select {}
+}
